54ffc936b93f4968ae09c08d4fbb58a1b4439829
MichaelAffanyi <michael.affanyi@amalitech.com>
2023-01-12 16:25:14 +0000

    Add home

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,27 +1,14 @@
 import React from 'react'
-import {
-    Container,
-    Content
-} from './HomeStyles'
+import HomeHead from '../../Components/HomeHead/HomeHead'
+// import {
+//     Container,
+//     Content
+// } from './HomeStyles'
 
 function Home() {
   return (
     <div>
-       <Container>
-        <img src='./assets/bg-pattern-home-1.svg' className='bg_1' alt='' />
-        <Content>
-            <div className='title'>
-                <span>Find the <br/> best <span className='special'>talent</span></span>
-            </div>
-            <div className='article'>
-                <div className='line'></div>
-                <div className='content'>
-                    <span>Finding the right people and building high performing teams can be hard. Most companies aren’t tapping into the abundance of global talent. We’re about to change that.</span>
-                </div>
-            </div>
-        </Content>
-        <img src='./assets/bg-pattern-home-2.svg' className='bg_2' alt='' />
-       </Container> 
+        <HomeHead />
     </div>
   )
 }
